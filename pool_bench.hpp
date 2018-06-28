78a4a5caea401d88fac66b401affe0db123ba35f
Ray Kim <msca8h@naver.com>
2018-06-29 02:25:20 +0900

    initial commit

--- pool_bench.hpp
+++ pool_bench.hpp
@@ -1,4 +1,22 @@
 
+/* 
+ * thread-pool-benchmark, a C++ Thread Pool Colosseum
+ * Copyright (C) 2018 Red-Portal
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _POOL_BENCH_POOL_BENCH_
 #define _POOL_BENCH_POOL_BENCH_
 
